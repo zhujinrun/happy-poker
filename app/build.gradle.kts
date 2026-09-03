59ee648f8274b96c19e22d52b0c4e1e94fb1e6d3
zhujinrun <13235578006@163.com>
2026-09-03 22:30:54 +0800

    更新 1.0.4 发布信息

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,8 +29,8 @@ android {
         applicationId = "com.happy.poker.app"
         minSdk = 24
         targetSdk = 35
-        versionCode = 4
-        versionName = "1.0.3"
+        versionCode = 5
+        versionName = "1.0.4"
     }
 
     signingConfigs {
